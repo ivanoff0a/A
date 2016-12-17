a757620d4ff67c3442b2cbb81e0999c15c844431
ivanoff0a <ivanov-andrey15@mail.ru>
2016-12-17 20:30:39 +0300

    hw

--- app/src/main/java/ru/banana/andrew/gameroom/Videogame.java
+++ app/src/main/java/ru/banana/andrew/gameroom/Videogame.java
@@ -9,12 +9,20 @@ public class Videogame implements Serializable {
     String description;
     String photo;
     String screenshot;
+    String title;
+    //String screenshot2;
+    //String screenshot3;
+    //String screenshot4;
 
 
-    Videogame(String description, String photo,String screenshot) {
+    Videogame(String description, String photo,String screenshot, String title/*String screenshot2, String screenshot3, String screenshot4*/) {
         this.description = description;
         this.photo = photo;
         this.screenshot = screenshot;
+        this.title = title;
+        //this.screenshot2 = screenshot2;
+        //this.screenshot3 = screenshot3;
+        //this.screenshot4 = screenshot4;
     }
 
 
--- app/src/main/java/ru/banana/andrew/gameroom/VideogameSecondActivity.java
+++ app/src/main/java/ru/banana/andrew/gameroom/VideogameSecondActivity.java
@@ -1,8 +1,10 @@
 package ru.banana.andrew.gameroom;
 
 import android.media.Image;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,19 +20,33 @@ public class VideogameSecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_videogame_second);
 
         Videogame videogames = (Videogame) getIntent().getSerializableExtra("VIDEOGAME");
-        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+        ImageView imageView = (ImageView) findViewById(R.id.ImageView1);
         Picasso.with(this).load(videogames.photo).fit().centerCrop().into(imageView);
 
-        ImageView imageView2 = (ImageView) findViewById(R.id.imageView3);
+        ImageView imageView2 = (ImageView) findViewById(R.id.ImageView2);
         Picasso.with(this).load(videogames.screenshot).fit().centerCrop().into(imageView2);
 
 
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(videogames.description);
 
-        ImageView wikiimage = (ImageView) findViewById(R.id.imageView2);
+        ImageView wikiimage = (ImageView) findViewById(R.id.imageView);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.mipmap.ic_arrow);
+        actionBar.setTitle(videogames.title);
     }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+
+        return true;
+    }
+
 }
 
 
