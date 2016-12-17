4202c87a650a79a43c0126d9732bd005e5e5cc88
ivanoff0a <ivanov-andrey15@mail.ru>
2016-12-17 20:52:36 +0300

    hw2

--- app/src/main/java/ru/banana/andrew/gameroom/Videogame.java
+++ app/src/main/java/ru/banana/andrew/gameroom/Videogame.java
@@ -10,20 +10,23 @@ public class Videogame implements Serializable {
     String photo;
     String screenshot;
     String title;
-    //String screenshot2;
-    //String screenshot3;
-    //String screenshot4;
+    String screenshot2;
+    String screenshot3;
+    String screenshot4;
 
 
-    Videogame(String description, String photo,String screenshot, String title/*String screenshot2, String screenshot3, String screenshot4*/) {
+    Videogame(String description, String photo,String screenshot) {
+        this.description = description;
+        this.photo = photo;
+        this.screenshot = screenshot;
+    }
+    Videogame(String description, String photo,String screenshot, String title, String screenshot2, String screenshot3, String screenshot4) {
         this.description = description;
         this.photo = photo;
         this.screenshot = screenshot;
         this.title = title;
-        //this.screenshot2 = screenshot2;
-        //this.screenshot3 = screenshot3;
-        //this.screenshot4 = screenshot4;
+        this.screenshot2 = screenshot2;
+        this.screenshot3 = screenshot3;
+        this.screenshot4 = screenshot4;
     }
-
-
 }
