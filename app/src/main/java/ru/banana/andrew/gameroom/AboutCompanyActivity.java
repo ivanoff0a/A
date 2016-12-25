0f0e4bec8ff21e90e25c8ad023e69379da6ff62c
ivanoff0a <ivanov-andrey15@mail.ru>
2016-12-25 13:20:28 +0300

    r

--- app/src/main/java/ru/banana/andrew/gameroom/AboutCompanyActivity.java
+++ app/src/main/java/ru/banana/andrew/gameroom/AboutCompanyActivity.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ImageView;
 
@@ -17,19 +18,25 @@ public class AboutCompanyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_company);
 
-        ImageView imageView2 = (ImageView) findViewById(R.id.ImageView2);
-        Picasso.with(this).load("https://hh.ru/employer-tab-picture-resized/162341.png").fit().centerCrop().into(imageView2);
+        ImageView imageView2 = (ImageView) findViewById(R.id.imageView2);
+        Picasso.with(this).load("https://hh.ru/employer-tab-picture-resized/162341.png").fit().into(imageView2);
 
-        // заполняем активность
-        // - иконку
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-        //actionBar.setHomeAsUpIndicator(R.drawable.);
-        // - заголовок и подзаголовок тулбара
+        actionBar.setHomeAsUpIndicator(R.mipmap.ic_arrow);
         actionBar.setTitle("О компании");
-        // - информация об авторе
-
-
-
     }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+
+        return true;
+    }
+
+
+
 }
+
