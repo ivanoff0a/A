0b951ff8e6ff3edb9cfe928fab53dd4b7f2b9433
ivanoff0a <ivanov-andrey15@mail.ru>
2017-02-01 15:50:29 +0300

    е

--- app/src/main/java/ru/banana/andrew/gameroom/MyHappyReceiver.java
+++ app/src/main/java/ru/banana/andrew/gameroom/MyHappyReceiver.java
@@ -3,11 +3,20 @@ package ru.banana.andrew.gameroom;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MyHappyReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
         context.startActivity(new Intent(context, OnStartActivity.class));
+        Toast.makeText(context, intent.getAction(), Toast.LENGTH_LONG).show();
+        Log.v("Сообщение", intent.getAction());
+
+        Intent startActivityIntent = new Intent(context, VideogamesActivity.class);
+        startActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(startActivityIntent);
     }
+
 }
+
